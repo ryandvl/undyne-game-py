0582b9625d8b74dfc84c39e10e2789b6d1589757
Ryan <126920703+ryandvl@users.noreply.github.com>
2024-08-20 14:59:38 -0300

    project creation

---
+++ run.py
@@ -0,0 +1,7 @@
+import pygame
+
+from src.controllers.game_controller import GameController
+
+if __name__ == '__main__':
+    game = GameController()
+    game.start()
---
+++ src/controllers/game_controller.py
@@ -0,0 +1,5 @@
+import pygame
+
+class GameController:
+    def start(self):
+        return
